efe9e005dbb332dfd3353ca65ce31bf9e2b01de2
ejanalysis <info@ejanalysis.com>
2021-12-30 22:20:18 -0500

    add localdata.rds as data instead of creating it on the fly
    note that the package is referred to as blockdata but the repo is named EJAM-Blockdata. may need to fix that.

---
+++ R/localtree.R
@@ -0,0 +1,6 @@
+#' @name localtree
+#' @docType data
+#' @title quad tree format data on locations of US blocks
+#' @details created by \cr
+#'   localtree <- SearchTrees::createTree(quaddata, treeType = "quad", dataType = "point")
+NULL
